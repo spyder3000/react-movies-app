dbb33b2590bf186fae4a154cab7f48c0e3401eb2
jtv3000 <60490523+spyder3000@users.noreply.github.com>
2020-09-26 10:36:34 -0400

    small change

--- src/components/MovieListFilters.js
+++ src/components/MovieListFilters.js
@@ -61,6 +61,27 @@ export class MovieListFilters extends React.Component {
                 <option value="2002">2002</option>
                 <option value="2001">2001</option>
                 <option value="2000">2000</option>
+                <option value="1999">1999</option>
+                <option value="1998">1998</option>
+                <option value="1997">1997</option>
+                <option value="1996">1996</option>
+                <option value="1995">1995</option>
+                <option value="1994">1994</option>
+                <option value="1993">1993</option>
+                <option value="1992">1992</option>
+                <option value="1991">1991</option>
+                <option value="1990">1990</option>
+                <option value="1989">1989</option>
+                <option value="1988">1988</option>
+                <option value="1987">1987</option>
+                <option value="1986">1986</option>
+                <option value="1985">1985</option>
+                <option value="1984">1984</option>
+                <option value="1983">1983</option>
+                <option value="1982">1982</option>
+                <option value="1981">1981</option>
+                <option value="1980">1980</option>
+                <option value="Classic">Classic</option>
               </select>     
             </div>
             <div className="input-group__item">
